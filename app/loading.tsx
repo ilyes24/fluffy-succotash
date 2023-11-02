9db7c7162b2037e323ebdbe80be2878053c2c3b2
Ilyas Ghomrani <ghomraniilyas@gmail.com>
2023-11-02 14:47:08 +0100

    ui: adding loader

---
+++ app/loading.tsx
@@ -0,0 +1,9 @@
+import { Spin } from "antd";
+
+export default function Laoding() {
+  return (
+    <div className="w-full h-full flex items-center justify-center">
+      <Spin />
+    </div>
+  );
+}
