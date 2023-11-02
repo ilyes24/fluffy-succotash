7bdaf4c94e6bad6b578544cb8c3c267d24c4bbeb
Ilyas Ghomrani <ghomraniilyas@gmail.com>
2023-11-02 14:49:20 +0100

    ui: Herosection component

---
+++ components/HeroSection/index.tsx
@@ -0,0 +1,28 @@
+import { Button } from "antd";
+import Image from "next/image";
+
+export const HeroSection: React.FC = () => {
+  return (
+    <div className="w-full h-[25rem] relative items-center flex">
+      <Image
+        className="rounded-2xl"
+        src="/heroSection.png"
+        alt="Hero Section"
+        fill={true}
+      />
+      <div className=" w-6/12 h-3/4 absolute right-0">
+        <div className="z-1 bg-accent-light w-full h-full opacity-70 rounded-tl-2xl rounded-bl-2xl absolute"></div>
+        <div className="relative px-20 py-10 flex flex-col gap-3">
+          <p className="m-0 font-extrabold text-6xl text-primary">
+            Carry your Funk
+          </p>
+          <p className="m-0 text-2xl leading-9 text-primary">
+            Trendy handbags collection for your party animal
+          </p>
+
+          <Button type="primary" className="w-fit px-5">→ {'  '}See more</Button>
+        </div>
+      </div>
+    </div>
+  );
+};
