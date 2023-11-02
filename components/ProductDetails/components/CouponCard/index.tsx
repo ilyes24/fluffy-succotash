2220283a94aa892ca228de527434a1c66d42b057
Ilyas Ghomrani <ghomraniilyas@gmail.com>
2023-11-02 14:50:53 +0100

    UI: create CouponCard component

---
+++ components/ProductDetails/components/CouponCard/index.tsx
@@ -0,0 +1,26 @@
+import Link from "next/link";
+
+export type CouponCardProps = {
+    title: string;
+    termsLink: string;
+    code: string;
+};
+
+export const CouponCard: React.FC<CouponCardProps> = ({title, termsLink, code}: CouponCardProps) => {
+    return (
+        <div>
+            <div className="flex flex-row justify-between px-3 py-2 border-2 rounded-xl w-[25rem] border-primary">
+              <div className="flex-1">
+                <p className="break-normal">
+                  {title}
+                </p>
+                <Link href={termsLink}><p className="text-primary text-sm">Terms & Conditions</p></Link>
+              </div>
+              <div className="min-w-[7rem] cursor-pointer min-h-[4rem] bg-accent flex items-center justify-center flex-col">
+                <p className="text-accent">Use Code</p>
+                <p className="text-base">{code}</p>
+              </div>
+            </div>
+          </div>
+    );    
+};
