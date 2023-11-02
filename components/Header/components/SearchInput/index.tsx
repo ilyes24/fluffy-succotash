a18a6a1568af91deddfd801a7c51bfba44042640
Ilyas Ghomrani <ghomraniilyas@gmail.com>
2023-11-02 14:52:46 +0100

    UI: header search input

---
+++ components/Header/components/SearchInput/index.tsx
@@ -0,0 +1,22 @@
+import Image from "next/image";
+import { Input } from "antd";
+
+export const SearchInput: React.FC = () => {
+  return (
+    <form>
+      <Input
+        className="w-96"
+        size="large"
+        placeholder="Search for products or brands....."
+        prefix={
+          <Image
+            src={"/search.svg"}
+            alt="Search"
+            width={20}
+            height={20}
+          />
+        }
+      />
+    </form>
+  );
+};
